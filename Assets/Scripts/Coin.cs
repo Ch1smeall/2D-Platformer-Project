c16222e2f00ec2ae4e13900a6da3d4adc0f31cd1
Ch1smeall <topherchrissmeall@gmail.com>
2025-12-02 20:41:43 -0500

    Added Collectibles (Coins) + Animation

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+   private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player"){
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+
+            if (player != null)
+            {
+                player.coins += 1;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
