bffcbf7bb06ba937d545261808023a1a25dbbec1
Chris Smeall <topherchrissmeall@gmail.com>
2025-12-11 17:34:18 -0500

    Revert "Feature sound effects"

--- Assets/Scripts/WinZone.cs
+++ Assets/Scripts/WinZone.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class WinZone : MonoBehaviour
 {
-    
+    public WinUIController winUI;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the thing that touched this is the Player
+        if (collision.CompareTag("Player"))
+        {
+            if (winUI != null)
+            {
+                winUI.ShowWinScreen();
+            }
+        }
+    }
 }
